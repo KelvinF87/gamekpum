d4d022943562f0d4ac171238da61dd94abbe7dfd
KelvinFamilia <kelvinjosefamilia@gmail.com>
2025-01-28 11:43:07 +0000

    a;adi el control de disparo y vidas y pagina de ayuda

--- js/juego.js
+++ js/juego.js
@@ -6,44 +6,47 @@ class Juego {
         this.correctAnswers = 0;
         this.currentQuestionIndex = 0;
     }
+
     getQuestion() {
         return this.preguntas[this.currentQuestionIndex];
     }
+
     moveToNextQuestion() {
         this.currentQuestionIndex++;
     }
+
     shuffleQuestions() {
         this.preguntas = this.preguntas.sort(() => Math.random() - 0.5);
     }
+
+    shuffleRespuesta() {
+        const currentQuestion = this.getQuestion();
+        if (currentQuestion) {
+            currentQuestion.shuffleChoices();
+        }
+    }
+
     checkAnswer(answer) {
         if (answer === this.getQuestion().answer) {
             this.correctAnswers++;
-
         }
     }
+
     hasEnded() {
-
-        if (this.currentQuestionIndex < this.preguntas.length) {
-            return false;
-        } else if (this.currentQuestionIndex == this.preguntas.length) {
-            return true;
-        }
-
+        return this.currentQuestionIndex >= this.preguntas.length;
     }
+
     filterQuestionsByDifficulty(difficulty) {
         if (difficulty > 0 && difficulty <= 3) {
             this.preguntas = this.preguntas.filter((pregunta) => pregunta.difficulty === difficulty);
         }
-        console.log(this.preguntas)
+        console.log(this.preguntas);
     }
+
     averageDifficulty() {
-
         const averageDifficulty = this.preguntas.reduce((total, pregunta) => total + pregunta.difficulty, 0) / this.preguntas.length;
-
-        console.log(averageDifficulty)
+        console.log(averageDifficulty);
         return averageDifficulty;
-
     }
-
-
 }
+
--- js/preguntas.js
+++ js/preguntas.js
@@ -4,11 +4,8 @@ class Preguntas {
         this.choices = choices;
         this.answer = answer;
         this.difficulty = difficulty;
-
     }
-
     shuffleChoices(){
-         this.choices = this.choices.sort(() => Math.random() - 0.5);
-     
+        this.choices = this.choices.sort(() => Math.random() - 0.5);
     }
 }
